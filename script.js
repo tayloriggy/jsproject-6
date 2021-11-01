3bc4b1bd2b1ba587ad699c3886e6a0f7e4f434a7
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-10-31 21:58:16 -0500

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+-Create an HTML form with a three-question quiz
+-Each question should have four multiple-choice answers to choose from.
+-When the user submits the quiz, add a message underneath each question letting them know 
+if they got it right/wrong (give them the correct answer).
+
+
+
+*/
